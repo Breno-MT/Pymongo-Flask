d6b05f2a7633d8a963aa98d03d7fd84e39f72572
Breno-MT <hentgenp@gmail.com>
2022-10-25 20:30:33 -0300

    [BRENO] REFACT: init app

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import os
-from src.app import create_app
+from src.app import app
 from src.app.routes import routes
 
-app = create_app(os.getenv("FLASK_ENV"))
 routes(app)
 
 if __name__ == "__main__":
