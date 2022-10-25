c5c275414df5cdb78979d57829bcd0c6b65a2bf5
Breno-MT <hentgenp@gmail.com>
2022-10-25 10:49:10 -0300

    [BRENO] REFACT: update user controller

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, jsonify
-from src.app import mongo
+import json
+from bson import json_util
+from flask import Blueprint
+from flask.wrappers import Response
+from src.app import DB
 
 
 users= Blueprint("user", __name__, url_prefix="/users")
@@ -7,11 +10,14 @@ users= Blueprint("user", __name__, url_prefix="/users")
 @users.route("/", methods=["GET"])
 def list_all_users():
     
-    test_query = mongo.db.users.find({})
+    dbname = DB.cx.get_database("BusPeople")
+    collection_name = dbname["passagem"]
 
-    for x in test_query:
+    user = collection_name.find()
 
-        return jsonify(x), 200
 
-    return jsonify(test_query), 200
-
+    return Response(
+        response=json.dumps(json_util.dumps(user)),
+        status=200,
+        mimetype="application/json"
+    )
