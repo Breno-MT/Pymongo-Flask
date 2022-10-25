feb4fb418bc4dd4238701331158be46dd008dd3b
Breno-MT <hentgenp@gmail.com>
2022-10-25 10:00:14 -0300

    [BRENO] REFACT: update app init

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,15 +1,16 @@
 import os
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from src.app.config import app_config
 
-mongo = PyMongo(uri=os.getenv("MONGO_URI"))
+DB = PyMongo(uri=os.getenv("MONGO_URI"))
 
 def create_app(enviroment):
     app = Flask(__name__)
     app.config.from_object(app_config[enviroment])
-    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-    mongo.init_app(app)
+    DB.init_app(app)
+    CORS(app)
 
     return app
 
