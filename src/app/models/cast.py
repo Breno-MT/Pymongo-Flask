4c326aa4252a4d46fdf38f13c8de455ea84c6b2d
Breno-MT <hentgenp@gmail.com>
2022-10-25 21:53:21 -0300

    [BRENO] REFACT: error message

--- src/app/models/cast.py
+++ src/app/models/cast.py
@@ -15,6 +15,6 @@ def create_collection_cast(mongo_client):
     try:
         mongo_client.create_collection("cast")
     except Exception as e:
-        print(e)
+        print(f"Something went wrong! {e}")
 
     mongo_client.command("collMod", "cast", validator=cast_validator)
