b005dd5497bfb43a7ad5e5892ed3f9dbac93ba81
Breno-MT <hentgenp@gmail.com>
2022-10-25 21:40:59 -0300

    [BRENO] FEAT: recode init

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,12 +3,15 @@ from flask import Flask
 from flask_cors import CORS
 from src.app.config import app_config
 from src.app.utils import mongo
+from src.app.models.movie import create_collection_movies
+from src.app.models.cast import create_collection_cast
 
 app = Flask(__name__)
 app.config.from_object(app_config[os.getenv("FLASK_ENV")])
 mongo.init_app(app)
-mongo_client = mongo.db
+mongo_client = mongo.cx.get_database("netflix")
+
+create_collection_movies(mongo_client)
+create_collection_cast(mongo_client)
 
 CORS(app)
-
-
