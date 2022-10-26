04a33673b579989bfa3f12381e99bfd824213dea
Breno-MT <hentgenp@gmail.com>
2022-10-25 21:40:37 -0300

    [BRENO] FEAT: models

---
+++ src/app/models/movie.py
@@ -0,0 +1,42 @@
+def create_collection_movies(mongo_client):
+    movies_validator = {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "properties": {
+                "type": {
+                    "bsonType": "string",
+                    "description": "If its either a Movie or TV Show",
+                    "enum": ["Movie", "TV Show"]
+                },
+                "title": {
+                    "bsonType": "string",
+                    "description": "Movies or TV Shows title"
+                },
+                "release_year": {
+                    "bsonType": "int",
+                    "description": "Movies or TV Shows release year"
+                },
+                "description": {
+                    "bsonType": "string",
+                    "description": "Description of the Movies or TV Shows"
+                },
+                "cast": {
+                    "bsonType": "array",
+                    "description": "Movie cast",
+                    "items": {
+                        "bsonType": "string",
+                        "description": "Person from the Movie or TV Show"
+                    }
+                }
+            }
+        }
+    }
+
+    try:
+        mongo_client.create_collection("movies")
+        print("Database created successfully!")
+    
+    except Exception as e:
+        print(f"Something went wrong! {e}")
+
+    mongo_client.command("collMod", "movies", validator=movies_validator)
