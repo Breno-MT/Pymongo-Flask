b61ad9feb2ef83a63ad155ca09c492030fd83ef4
Breno-MT <hentgenp@gmail.com>
2022-10-26 21:37:53 -0300

    [BRENO] REFACT: import function

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from src.app.config import app_config
 from src.app.utils import mongo
 from src.app.models.movie import create_collection_movies
-from src.app.models.cast import create_collection_cast
+from app.models.comments import create_collection_comments
 
 app = Flask(__name__)
 app.config.from_object(app_config[os.getenv("FLASK_ENV")])
@@ -12,6 +12,6 @@ mongo.init_app(app)
 mongo_client = mongo.cx.get_database("netflix")
 
 create_collection_movies(mongo_client)
-create_collection_cast(mongo_client)
+create_collection_comments(mongo_client)
 
 CORS(app)
