b4fe27be365bc1320d9ca1b129c08e08981bae63
Breno-MT <hentgenp@gmail.com>
2022-10-26 21:47:41 -0300

    [BRENO] REFACT: created cli command

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,8 @@
 import os
+import click
 from flask import Flask
 from flask_cors import CORS
+from flask.cli import with_appcontext
 from src.app.config import app_config
 from src.app.utils import mongo
 from src.app.models.movie import create_collection_movies
@@ -11,7 +13,12 @@ app.config.from_object(app_config[os.getenv("FLASK_ENV")])
 mongo.init_app(app)
 mongo_client = mongo.cx.get_database("netflix")
 
-create_collection_movies(mongo_client)
-create_collection_comments(mongo_client)
+@click.command(name="create_collections")
+@with_appcontext
+def create_collections():
+    create_collection_movies(mongo_client)
+    create_collection_comments(mongo_client)
+
+app.cli.add_command(create_collections)
 
 CORS(app)
