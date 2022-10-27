7acf695c00b83dbc1edcbbfa92865961b35dea5d
Breno-MT <hentgenp@gmail.com>
2022-10-26 21:39:29 -0300

    [BRENO] FIX: import error

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from src.app.config import app_config
 from src.app.utils import mongo
 from src.app.models.movie import create_collection_movies
-from app.models.comments import create_collection_comments
+from src.app.models.comments import create_collection_comments
 
 app = Flask(__name__)
 app.config.from_object(app_config[os.getenv("FLASK_ENV")])
